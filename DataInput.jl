3443bcfaecf42a41da20973f6a8cf0bebb316766
yidai1996 <59218753+yidai1996@users.noreply.github.com>
2022-12-08 16:54:15 -0500

    Add customed importing data code

---
+++ DataInput.jl
@@ -0,0 +1,4 @@
+using CSV
+# df = readtable("G:\\My Drive\\Research\\SVM\\New folder\\3R parallel to 2and1 parallel +6K.csv")
+# df = readtable("3R parallel to 2and1 parallel +6K.csv")
+CSV.read("G:\\My Drive\\Research\\SVM\\New folder\\3R parallel to 2and1 parallel +6K.csv",DataFrame)
